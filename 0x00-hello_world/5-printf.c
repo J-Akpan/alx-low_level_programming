638dde731cc23a41501b7e695b9f0f6e78c1d4e1
J-Akpan <jakpan64@yahoo.com>
2023-03-16 16:47:29 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * Description: main - program to print out string.
+ * Return: always return 0 if successfull.
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}

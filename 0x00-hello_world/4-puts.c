37add4389c3491234c9552aeb1f32ac6493e1f7c
J-Akpan <jakpan64@yahoo.com>
2023-03-16 16:14:47 -0700

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,5 @@
 #include <stdio.h>
 
-/*
- * Description: main - Description for main program
- *
- * Return: always 0 on success
- */
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");

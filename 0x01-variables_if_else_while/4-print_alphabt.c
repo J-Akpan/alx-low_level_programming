40a5bd16ddc82bedc11f477d4d7f769893538302
J-Akpan <jakpan64@yahoo.com>
2023-03-17 16:27:21 -0700

    4-print_alphabt

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -12,7 +12,7 @@ int main(void)
 
 	for (letters = 'a'; letters <= 'z'; letters++)
 	{
-		if (letters != 'e' || letters != 'q')
+		if (letters != 'e' && letters != 'q')
 		{
 			putchar(letters);
 		}

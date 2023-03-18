d9934cdcb978423e0a79348995004d7cebe8372a
J-Akpan <jakpan64@yahoo.com>
2023-03-17 17:07:38 -0700

    5-print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * Description: main - Program tof print numbers
+ *
+ * Return: 0 if success
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		printf("%d \n", i);
+	}
+	return (0);
+}

86f880be46b663502675753fe62e3748c3273abd
J-Akpan <jakpan64@yahoo.com>
2023-03-17 21:03:06 -0700

    100-print_comb3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * Description: main - printing of  combinTIONS.
+ *
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	int a;
+	int b;
+
+	for (a = 0; a < 9; a++)
+	{
+		for (b = a + 1; b < 10; b++)
+		{
+			putchar('0' + a);
+			putchar('0'+ b);
+
+			if (a == 8 && b ==9 )
+			{
+				continue;
+			}
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}

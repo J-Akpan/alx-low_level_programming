727b8fde2ff63889992c9dba1b9ae4ca19d5e471
J-Akpan <jakpan64@yahoo.com>
2023-03-23 15:40:48 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,20 +2,21 @@
 
 /**
  * Description: _isupper - compare codes.
+ *
  * @c: character to be checked
- * Return: 0 always.
+ *
+ * Return: 1 if c is uppercase otherwise return 0
  */
 
 int _isupper(int c)
 {
 	char c;
 
-	if (c >= 'A' || c <= 'Z')
+	for (c = 'A'; c <= 'Z'; c++)
 	{
-		return (0);
-	}
-	else
-	{
-		return (1);
+		if (c >= 'A' && c <= 'Z')
+			return (1);
+		else
+			return (0);
 	}
 }

2be379c9c57be81cd58be7e70a631a1f131ccec7
J-Akpan <jakpan64@yahoo.com>
2023-03-23 16:10:10 -0700

    0-isup

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -10,13 +10,8 @@
 
 int _isupper(int c)
 {
-	char c;
-
-	for (c = 'A'; c <= 'Z'; c++)
-	{
-		if (c >= 'A' && c <= 'Z')
-			return (1);
-		else
-			return (0);
-	}
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
 }

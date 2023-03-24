5d4f98199afa2f5bc95aa63e96eb5f680287be0e
J-Akpan <jakpan64@yahoo.com>
2023-03-23 20:31:08 -0700

    5-more_numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * Description: more_numbers - to multiply numbers by 10
+ * Return: 0 on success
+ */
+
+void more_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 14; i++)
+	{
+		_putchar(i * 10);
+	}
+	_putchar('\n');
+}

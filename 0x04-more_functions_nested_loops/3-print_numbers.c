bc159fd580772d1ef84ca346ae8f26bbe3602dd6
J-Akpan <jakpan64@yahoo.com>
2023-03-23 18:36:45 -0700

    3-print_numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * Description: print_numbers - program to print numbers fro 0 - 9
+ * Return: 0 always
+ */
+
+void print_numbers(void)
+{
+	int c;
+
+	for (c = 0; c <= 9; c++)
+		_putchar(c);
+	_putchar('\n');
+}

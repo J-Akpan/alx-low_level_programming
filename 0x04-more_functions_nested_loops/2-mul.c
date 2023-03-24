82595f929ff8fc34b1a6504563516be241f94631
J-Akpan <jakpan64@yahoo.com>
2023-03-23 18:12:59 -0700

    2-mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * Description: mul - program to multultiply numbers
+ * @a: paramater to multiply
+ * @b: second parameter to be checked
+ * Return: 0 always
+ */
+
+int mul(int a, int b)
+{
+	int mult;
+
+	mult = a * b;
+	return (mult);
+}

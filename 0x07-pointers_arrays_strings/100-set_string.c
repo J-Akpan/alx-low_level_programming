f83b6c92ca126b89b77ac671a36eba2d9b0c284e
J-Akpan <jakpan64@yahoo.com>
2023-04-04 01:20:44 -0700

    100-set_string

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * Description: set_string - set pointers to char
+ *
+ * @s: double pointer params
+ * @to: pointer params
+ *
+ * Return: void
+ *
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}

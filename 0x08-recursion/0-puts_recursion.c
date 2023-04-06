ff882af6025b4d619bdb7a41e98d66e7ba4207f9
J-Akpan <jakpan64@yahoo.com>
2023-04-06 16:58:11 -0700

    0-puts_recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * Description: _puts_recursion - print strings
+ *
+ * @s: param 1
+ *
+ * Return: void
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+		return;
+
+	_puts_recursion(s + 1);
+}

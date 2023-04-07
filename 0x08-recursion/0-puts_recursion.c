65cb70e3dc284e889c163ce928c1d62f73403171
J-Akpan <jakpan64@yahoo.com>
2023-04-06 17:27:35 -0700

    0-puts_recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -14,9 +14,8 @@ void _puts_recursion(char *s)
 		return;
 	else if (*s)
 	{
-	_putchar(*s);
-	_puts_recursion(s + 1);
-	_putchar('\n');
+		_putchar(*s);
+		_puts_recursion(s + 1);
 	}
 	else
 		_putchar('\n');

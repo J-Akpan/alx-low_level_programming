f1775991b07ca85fadea4282c5df86e4a1571ce1
J-Akpan <jakpan64@yahoo.com>
2023-04-07 03:42:56 -0700

    1-print_rev_recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * Description: _print_rev_recursion - program to print in reverse
+ *
+ * @s: param1
+ *
+ * Return: 0 always
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+	else if (*s)
+	{
+		_putchar(*s);
+		_print_rev_recursion(s - 1);
+	}
+	else
+		_putchar('\n');
+}

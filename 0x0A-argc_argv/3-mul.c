05d859c8d648fc18432e4ec72016fa1b9b583830
J-Akpan <jakpan64@yahoo.com>
2023-04-08 16:03:03 -0700

    3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * Description: main - program to multiply two numbers
+ *
+ * @argv: param 1
+ * @argc: param 2
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+	int multiply = 1;
+
+	for (i = 1; i < argc; i++)
+	{
+		int y = strtol(argv[i], NULL, 10);
+
+		multiply = multiply * y;
+	}
+	printf("%d\n", multiply);
+	return (0);
+}

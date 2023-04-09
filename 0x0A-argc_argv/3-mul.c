c2f21c0daabb54f8ad7ac576848f6b05ae0b361c
J-Akpan <jakpan64@yahoo.com>
2023-04-09 08:05:29 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,29 +12,20 @@
 
 int main(int argc, char *argv[])
 {
-	int i;
-	int multiply = 1;
+	int multiply;
 
-	if (argc == 1)
+	if (argc == 3)
+	{
+		int y = strtol(argv[1], NULL, 10);
+		int z = strtol(argv[2], NULL, 10);
+
+		multiply = y * z;
+		printf("%d\n", multiply);
+	}
+	else
 	{
 		printf("Error\n");
 		return (1);
 	}
-	else if (argc == 2)
-	{
-		printf("Error\n");
-		return (1);
-	}
-	else if (argc == 3)
-	{
-		for (i = 1; i < argc; i++)
-		{
-			int y = strtol(argv[i], NULL, 10);
-			int z = strtol(argv[i], NULL, 10);
-
-			multiply = y * z;
-		}
-	}
-	printf("%d\n", multiply);
 	return (0);
 }

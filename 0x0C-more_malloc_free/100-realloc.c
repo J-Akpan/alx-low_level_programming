844808993bdd183188042971c559fbfd4a9cf953
J-Akpan <jakpan64@yahoo.com>
2023-04-14 19:36:16 -0700

    100-realloc

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,55 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * Description: *_realloc - fn to reallocate memory
+ *
+ * @ptr: parma 1
+ * @old_size: param 2
+ * @new_size: param 3
+ *
+ * Return: positive numbers
+ */
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+
+	char *ptr1;
+	char *old_ptr;
+	unsigned int i;
+
+	if (new_size == old_size)
+		return (ptr);
+
+	if (new_size == 0 && ptr)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	if (!ptr)
+		return (malloc(new_size));
+	ptr1 = malloc(new_size);
+
+	if (!ptr1)
+		return (NULL);
+
+	old_ptr = ptr;
+
+	if (new_size < old_size)
+	{
+		for (i = 0; i < new_size; i++)
+			ptr1[i] = old_ptr[i];
+	}
+
+	if (new_size > old_size)
+	{
+		for (i = 0; i < old_size; i++)
+
+			ptr1[i] = old_ptr[i];
+	}
+	free(ptr);
+	return (ptr1);
+
+}

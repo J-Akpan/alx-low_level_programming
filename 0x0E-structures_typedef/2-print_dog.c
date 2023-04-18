bd6a8ee57b186720ecc820ad561c8b57546f83ac
J-Akpan <jakpan64@yahoo.com>
2023-04-17 23:36:45 -0700

    2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <stdio.h>
 
 /**
  * print_dog - functikon to rint dog structure
@@ -10,13 +11,14 @@
 
 void print_dog(struct dog *d)
 {
-	/*stm to declear nil*/
+	/*stmt to declare nil*/
 	char *nil = "(nil)";
 
 	/*check if d is NULL*/
 	if (d == NULL)
 		return;
-	/*print the members of the structure*/
+
+	/*check to see if memeber is nil, else print member*/
 	printf("Name:%s\n", (d->name) ? d->name : nil);
 	printf("Age: %f\n", d->age);
 	pritf("Owner: %s\n", (d->owner) ? d->owner : nil);

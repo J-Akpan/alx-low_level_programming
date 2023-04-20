d973db7768686e9e1a7864db39f2a86b95baf0b8
J-Akpan <jakpan64@yahoo.com>
2023-04-19 23:10:46 -0700

    2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -14,6 +14,8 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int m;
 
+	if (array == NULL || size <= 0 || cmp == NULL)
+
 	for (m = 0; m < size; m++)
 		if (cmp(array[m]))
 			return (m);

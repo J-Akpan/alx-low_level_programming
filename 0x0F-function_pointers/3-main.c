dc45a51dd122a2bf2afaeb5ee7464db0281472b9
J-Akpan <jakpan64@yahoo.com>
2023-04-20 08:18:56 -0700

    3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -12,9 +12,10 @@
  * Return: 0 on success
  */
 
-int main(int argc, char *argv[])
+int main(int__attribute__((__unused__)) argc, char *argv[])
 {
-	int (*opt)(int, int);
+	int num1, num2;
+	char *op;
 
 	if (argc != 4)
 	{
@@ -22,15 +23,23 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 
-	opt = get_op_func(argv[2]);
+	num1 = atoi(argv[1]);
+	op = argv[2];
+	num2 = atoi(argv[3]);
 
-	if (!opt)
+	if (get_op_func(op) == NULL || op[1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
 	}
 
-	printf("%d\n", opt(atoi(argv[1]), atoi(argv[2])));
+	if ((*op == '/' && num2 == 0) ||(*op == '%' && num2 == 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	print("%d\n", get_op_func(op)(num1, num2));
 
 	return (0);
 }

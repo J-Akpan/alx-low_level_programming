f1e5ccb5df9bc6f79136893dd903c14bbf829b22
J-Akpan <jakpan64@yahoo.com>
2023-04-28 01:45:41 -0700

    task 100

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+/*void first(void)__attribute__((constructor));*/
+
+/**
+ * Description: first - function to print sentence
+ *
+ */
+
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}

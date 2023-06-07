f39a26d36bda0c1aa42488451ffddb26458ebf87
Joseph Akpan <jakpan64@yahoo.com>
2023-06-07 08:20:01 -0700

    commit to task 1

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * Description: _puts_recursion - function to print strings 
+ *
+ * Returen: 0 on success
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+		_putchar('\n');
+	else
+	{
+		_putchar(*s);
+		_puts_recursion(++s);
+	}
+}

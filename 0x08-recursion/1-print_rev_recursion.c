5aecc7c7d37153e5481c8f80d64e8f4b559bb0e2
Joseph Akpan <jakpan64@yahoo.com>
2023-06-07 08:58:40 -0700

    task1 commit

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * Description: _print_rev_recursion - function to print reverse string
+ *
+ * @s: strint parameter to print
+ *
+ * Return: 0 on success
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+		_putchar('\n');
+	else
+	{
+		_print_rev_recursion(++s);
+		_putchar(*s);
+	}
+}

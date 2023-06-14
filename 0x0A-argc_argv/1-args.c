b5fd2358528f6388ee304dfc9972c21742eff3c7
Joseph Akpan <jakpan64@yahoo.com>
2023-06-14 07:43:56 -0700

    task1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include  "main.h"
+#include <stdio.h>
+
+/**
+ * Description: main - function that print the number of argument passed into it
+ *
+ * @argc: parameter 1
+ * @argv: parameter 2
+ *
+ * Return: 0 on success
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d\n",argc);
+	return (0);
+}
